e48bbba2edadbc9d2de137296246fe565dd345b5
Ziwana Rizwan Pramudia <ziwanarizwan@gmail.com>
2025-08-07 10:31:04 +0700

    admin tidak bisa diubah

--- resources/views/dashboard/masteruser/index.blade.php
+++ resources/views/dashboard/masteruser/index.blade.php
@@ -64,10 +64,10 @@
                             
                             <td>
                                 <a href="/masteruser/{{ $user->id }}" class="badge bg-info"><span data-feather="eye"></span></a>
-                                <a href="/masteruser/{{ $user->id }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
                                 @if ($user->role == 'Admin')
-                                    
+                                
                                 @else
+                                    <a href="/masteruser/{{ $user->id }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
                                     <form action="/masteruser/{{ $user->id }}" method="post" class="d-inline">
                                 @method('delete')
                                 @csrf
